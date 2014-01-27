2012d93f1ae7c60f12db529bff7ceae928f7324f
ash5 <asa1126ha@gmail.com>
2014-01-27 19:00:10 +0900

    2014/1/27

--- routes/answer.js
+++ routes/answer.js
@@ -9,19 +9,28 @@
 	//get database
 	var db = server.db("sampleDB");
 
+	//decide collection_name
+	var col1_name = "test";
+	var col2_name = "posts";
+		
 	//get some collections
-	var test = db.collection("test");
-	var posts = db.collection("posts");
+	var col1 = db.collection(col1_name);
+	var col2 = db.collection(col2_name);
 
 	var querystring = require("querystring");
 	
 exports.insert = function(req,res){
-
+	
+	console.log("q=",req.body);
+	try{
+	console.log("query=",req.body.q_query);
+	}catch(e){console.log("ERROR!!!");}
+	
 	// 解答の入力文字列をJSONオブジェクトに変換
  	try{
 		var data = JSON.parse(req.body.answer);	
 		console.log("json=",data);
-	}catch(e){
+	}catch(e){//エラーをフィードバック表示
 		console.error("parsing error",e);
 		 res.render('main', {
 			    comment: 'parsing error'
@@ -29,39 +38,51 @@ exports.insert = function(req,res){
 	}
 	
 	//コレクションにドキュメントを追加
-	test.insert(data);
+	col1.insert(data);
 	
 	/*
 	 * 入力したドキュメントに対してクエリを実行
 	 */
 	
 	//コレクションからカーソルオブジェクトを取得
-	var q = "test.find({'a':1})";
-	var cursor = eval(q);
-	//JSONファイルから実行
-	//$.getJSON('/question/question.json',function(json){
-	//	var q = json[0].query;
-	//	var cursor = eval(q);
+	//クエリの代入
+	var q = "col1."+req.body.q_query;
+	console.log("query==",q);
 
-	//});
-	/*
-	.fail(function(jqXHR,textStatus,errorThrown){
-		console.error("error"+textStatus);
-	});
-	*/
+//	try{
+		var cursor = eval(q);	
+/*		
+	}catch(e){
+		console.error("eval error",e);
+		 res.render('main', {
+			    comment: 'eval error'
+			  });
+	}
+*/	
 	
-	//var cursor = test.find({"a":1});
+//var tmp = col1.find(req.body.q_result);
+var tmp = "col1.find({"+req.body.q_result+"})";
+ tmp = eval(tmp);
+tmp.forEach(function(doc){
+	console.log("tmp=",doc);
 	
+});		
 	//カーソルをすべて表示
 	cursor.forEach(function(doc){
-		console.log(doc);
+		console.log("doc=",doc._id);
+		
+		var tmp = "col1.find({"+req.body.q_result+","+"\"_id\""+":"+doc._id+"})";
+     
+		console.log("t=",tmp);
+		tmp = eval(tmp);
+		/*
+		try{
+		col1.find(q_)	
+		}catch(e){console.log("ERROR!");}
+		*/
 	});
 	
-	
-	var tmp = 'console.log("abc");';
-		eval(tmp);
-	//res.end();
-		//mainに戻る。
+	//mainに戻る。
 		res.redirect('/main');
 };
 
